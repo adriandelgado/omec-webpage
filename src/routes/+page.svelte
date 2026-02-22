721ef3b94ef44f0bb106a88bea760224db0a891c
Adrian J. Delgado <git@adrianjdelgado.com>
2026-02-21 21:20:24 -0500

    feat: add logo OMEC and fix some lints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { resolve } from "$app/paths";
+	import logo_omec from "$lib/assets/logo-omec.png";
 
 	const links = [
 		{
@@ -25,17 +26,12 @@
 	] as const;
 </script>
 
-<!-- Header -->
 <header class="absolute inset-x-0 top-0 z-50">
 	<nav aria-label="Global" class="flex items-center justify-between p-6 lg:px-8">
 		<div class="flex lg:flex-1">
-			<a href="#" class="-m-1.5 p-1.5">
-				<span class="sr-only">Your Company</span>
-				<img
-					src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-					alt=""
-					class="h-8 w-auto"
-				/>
+			<a href={resolve("/")} class="-m-1.5 p-1.5">
+				<span class="sr-only">OMEC</span>
+				<img src={logo_omec} alt="" class="h-8 w-auto" />
 			</a>
 		</div>
 		<div class="flex lg:hidden">
@@ -78,13 +74,9 @@
 					class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
 				>
 					<div class="flex items-center justify-between">
-						<a href="#" class="-m-1.5 p-1.5">
-							<span class="sr-only">Your Company</span>
-							<img
-								src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-								alt=""
-								class="h-8 w-auto"
-							/>
+						<a href={resolve("/")} class="-m-1.5 p-1.5">
+							<span class="sr-only">OMEC</span>
+							<img src={logo_omec} alt="" class="h-8 w-auto" />
 						</a>
 						<button
 							type="button"
@@ -134,29 +126,19 @@
 		>
 			<div
 				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-				class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+				class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
 			></div>
 		</div>
 		<div class="py-24 sm:py-32 lg:pb-40">
 			<div class="mx-auto max-w-7xl px-6 lg:px-8">
 				<div class="mx-auto max-w-2xl text-center">
 					<h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-						Deploy to the cloud with confidence
+						Olimpiada Matemática Ecuatoriana
 					</h1>
 					<p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-						Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-						commodo. Elit sunt amet fugiat veniam occaecat.
+						Desarrollando el talento matemático de los estudiantes ecuatorianos y promoviendo la
+						excelencia académica
 					</p>
-					<div class="mt-10 flex items-center justify-center gap-x-6">
-						<a
-							href="#"
-							class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-							>Get started</a
-						>
-						<a href="#" class="text-sm/6 font-semibold text-gray-900"
-							>Learn more <span aria-hidden="true">→</span></a
-						>
-					</div>
 				</div>
 				<div class="mt-16 flow-root sm:mt-24">
 					<div
@@ -167,7 +149,7 @@
 							height="1442"
 							src="https://tailwindcss.com/plus-assets/img/component-images/project-app-screenshot.png"
 							alt="App screenshot"
-							class="w-[76rem] rounded-md bg-gray-50 shadow-xl ring-1 ring-gray-900/10"
+							class="w-304 rounded-md bg-gray-50 shadow-xl ring-1 ring-gray-900/10"
 						/>
 					</div>
 				</div>
@@ -179,7 +161,7 @@
 		>
 			<div
 				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-				class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+				class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
 			></div>
 		</div>
 	</div>
@@ -388,16 +370,16 @@
 			<div aria-hidden="true" class="absolute -top-56 -left-80 transform-gpu blur-3xl">
 				<div
 					style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-					class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]"
+					class="aspect-1097/845 w-274.25 bg-linear-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]"
 				></div>
 			</div>
 			<div
 				aria-hidden="true"
-				class="hidden md:absolute md:bottom-16 md:left-[50rem] md:block md:transform-gpu md:blur-3xl"
+				class="hidden md:absolute md:bottom-16 md:left-200 md:block md:transform-gpu md:blur-3xl"
 			>
 				<div
 					style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-					class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-25"
+					class="aspect-1097/845 w-274.25 bg-linear-to-r from-[#ff4694] to-[#776fff] opacity-25"
 				></div>
 			</div>
 			<div class="relative mx-auto max-w-2xl lg:mx-0">
@@ -527,7 +509,7 @@
 					<a
 						href="#"
 						aria-describedby="tier-freelancer"
-						class="mt-8 block rounded-md px-3 py-2 text-center text-sm/6 font-semibold text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						class="mt-8 block rounded-md px-3 py-2 text-center text-sm/6 font-semibold text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						>Buy plan</a
 					>
 				</div>
@@ -636,7 +618,7 @@
 					<a
 						href="#"
 						aria-describedby="tier-startup"
-						class="mt-8 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						class="mt-8 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						>Buy plan</a
 					>
 				</div>
@@ -740,7 +722,7 @@
 					<a
 						href="#"
 						aria-describedby="tier-enterprise"
-						class="mt-8 block rounded-md px-3 py-2 text-center text-sm/6 font-semibold text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						class="mt-8 block rounded-md px-3 py-2 text-center text-sm/6 font-semibold text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						>Buy plan</a
 					>
 				</div>
@@ -834,7 +816,7 @@
 		>
 			<div
 				style="clip-path: polygon(73.6% 48.6%, 91.7% 88.5%, 100% 53.9%, 97.4% 18.1%, 92.5% 15.4%, 75.7% 36.3%, 55.3% 52.8%, 46.5% 50.9%, 45% 37.4%, 50.3% 13.1%, 21.3% 36.2%, 0.1% 0.1%, 5.4% 49.1%, 21.4% 36.4%, 58.9% 100%, 73.6% 48.6%)"
-				class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-25"
+				class="aspect-1108/632 w-277 flex-none bg-linear-to-r from-[#ff80b5] to-[#9089fc] opacity-25"
 			></div>
 		</div>
 		<div class="mx-auto max-w-2xl text-center">
@@ -848,7 +830,7 @@
 			<div class="mt-10 flex items-center justify-center gap-x-6">
 				<a
 					href="#"
-					class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 					>Get started</a
 				>
 				<a href="#" class="text-sm/6 font-semibold text-gray-900"
@@ -862,7 +844,7 @@
 		>
 			<div
 				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-				class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+				class="aspect-1155/678 w-288.75 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
 			></div>
 		</div>
 	</div>
