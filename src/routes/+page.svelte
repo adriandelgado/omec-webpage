8153daae52282c6a7d5f9f9f3edc8d0c8aa818cd
Adrian J. Delgado <git@adrianjdelgado.com>
2026-04-11 15:49:17 -0500

    feat: add olympic calendar and international achievements sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import Hero from "./hero.svelte";
 	import ProgramasEntrenamiento from "./programas-entrenamiento.svelte";
+	import CalendarioOlimpico from "./calendario-olimpico.svelte";
+	import LogrosInternacionales from "./logros-internacionales.svelte";
 </script>
 
 <Hero />
 <ProgramasEntrenamiento />
+<CalendarioOlimpico />
+<LogrosInternacionales />
