08f4c529df5ea0fd1abfd24ac58a818a19d6fed3
Adrian J. Delgado <git@adrianjdelgado.com>
2026-04-21 19:20:24 -0500

    fix: ci stuff

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
 	schema: "./src/lib/server/db/schema.ts",
 	dialect: "turso",
 	dbCredentials: {
-		authToken: process.env.DATABASE_AUTH_TOKEN,
-		url: process.env.DATABASE_URL,
+		authToken: process.env.DATABASE_AUTH_TOKEN || "",
+		url: process.env.DATABASE_URL || "",
 	},
 	verbose: true,
 	strict: true,
