a70bdb49c04d2aab48b149a96e8b895090e4f1c1
Adrian J. Delgado <git@adrianjdelgado.com>
2026-08-30 20:10:43 -0500

    deployment: allow runtime env during build

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,11 @@
 import { defineEnvVars } from "@sveltejs/kit/env";
+import { building } from "$app/env";
+import * as v from "valibot";
 
 export const variables = defineEnvVars({
-	DATABASE_URL: { description: "The database connection string." },
+	DATABASE_URL: {
+		description: "The database connection string.",
+		// Cloudflare provides this Worker variable at runtime, not during the build.
+		schema: building ? v.optional(v.string()) : v.string(),
+	},
 });
