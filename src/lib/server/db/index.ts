73e6bab24b29d935bf7dac01256cf68d68755da8
Adrian J. Delgado <git@adrianjdelgado.com>
2026-09-16 18:57:28 -0500

    db: defer missing URL error during build

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,9 +2,13 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 import { DATABASE_AUTH_TOKEN, DATABASE_URL } from "$app/env/private";
+import { building } from "$app/env";
 
-if (!DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (!building && !DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
-const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN || undefined });
+const client = createClient({
+	url: DATABASE_URL || "file::memory:",
+	authToken: DATABASE_AUTH_TOKEN || undefined,
+});
 
 export const db = drizzle(client, { schema });
